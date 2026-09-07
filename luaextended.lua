e5bd17e13b4ca9ab25b8d7285cc270a8c272d96f
ejetaxeblevich <evgexayaransk@gmail.com>
2026-09-07 20:09:28 +0300

    2.4
    - added new funcs: string.isbinary, string.hex, table.tostring2, table.getn2;
    - fixed funcs: string.shield, table.tostring, table.amount2;
    - fixed LuaE.shield;
    - added LuaE.unshield and LuaE.hex;
    - added new Lua string. and table. aliases;
    - edit readme

--- luaextended.lua
+++ luaextended.lua
@@ -7,7 +7,7 @@
 --               написанный специально для игры
 --             Ex Machina / Hard Truck Apocalypse
 --
---                     LuaExtended v2.3
+--                     LuaExtended v2.4
 -- 
 -- 
 -- ===================== Автор E Jet ==========================
@@ -151,19 +151,24 @@
 --   [F] string  string.shield( string, bool Reverse )    /* Ставит или убирает экранирование спецсимволов в строке. Примеры: [string.shield("Текст?.+-%")] --> "Текст%?%.%+%-%%"; [string.shield("Текст%?%.%+%-%%", true)] --> "Текст?.+-%" */
 --   [F] table   string.totable( string Table )  /* Преобразует строку-таблицу в таблицу */
 --   [F] int     string.commas( string )   /* Возвращает количество запятых из строки */
+--   [F] string  string.isbinary( string ) /* Возвращает строку байт (форматированную), если строка содержит бинарные значения */
+--   [F] string  string.hex( string, string separator )   /* Преобразует строку как hex значения с разделением separator */
+--
 --
 --   /* Таблицы */
---   [F] string  table.debug( table )      /* Возвращает строку "распакованной" таблицы. Разворачивает все вложения, очень удобно для отладки таблицы в LOG() */
---   [F] table   table.copy( table )       /* Возвращает копию таблицы. В lua присвоение таблицы новой переменной НЕ РАВНО созданию копии этой таблицы: [local t = {}; local t2 = t	--> t и t2 одна и та же таблица, просто это разные ссылки на нее]; [local t = {}; local t2 = table.copy(t)	--> t и t2 разные таблицы] */
---   [F] bool    table.equal( table t1, table t2 )    /* Проверяет, являются ли таблицы одинаковыми. Поверхностно, для списков по числовым индексам */
---   [F] bool    table.equal2( table t1, table t2 )   /* Проверяет, являются ли таблицы одинаковыми. Рекурсивно, для словарей по всем индексам и ключам */
---   [F] bool    table.empty( table )      /* Проверяет, является ли таблица пустой */
---   [F] table   table.clear( table )      /* Очищает существующую таблицу, чтобы не создавать новую. Проходит по числовым индексам таблицы - в таблице не должно быть "дырок": [local t = {[1] = 1, [3] = 3} --> внутри таблицы t нет второго индекса (он уже nil)] = на этой дырке цикл остановится! */
---   [F] table   table.clear2( table )     /* Очищает существующую таблицу, чтобы не создавать новую. Проходит по всем индексам и ключам таблицы, очищая таблицу целиком вне зависимости от наличия "дырок", жертвуя скоростью по сравнению с [table.clear()] */
---   [F] string  table.tostring( table )   /* Преобразует таблицу в строку */
---   [F] bool    table.value( table, any value )   /* Проверяет, содержит ли таблица значение. Рекурсивно, для словарей по всем индексам и ключам */
---   [F] bool    table.key( table, string key )    /* Проверяет, содержит ли таблица ключ. Рекурсивно, для словарей по всем индексам и ключам */
---   [F] int     table.amount( table, any item )   /* Считает количество значений в таблице. Рекурсивно, для списков по числовым индексам таблицы */
+--   [F] string   table.debug( table )      /* Возвращает строку "распакованной" таблицы. Разворачивает все вложения, очень удобно для отладки таблицы в LOG() */
+--   [F] table    table.copy( table )       /* Возвращает копию таблицы. В lua присвоение таблицы новой переменной НЕ РАВНО созданию копии этой таблицы: [local t = {}; local t2 = t	--> t и t2 одна и та же таблица, просто это разные ссылки на нее]; [local t = {}; local t2 = table.copy(t)	--> t и t2 разные таблицы] */
+--   [F] int      table.getn2( table )      /* Возвращает количество элементов таблицы. Поверхностно, для словарей по всем индексам и ключам (стандартный getn считает только списки по индексам) */
+--   [F] bool     table.equal( table t1, table t2 )    /* Проверяет, являются ли таблицы одинаковыми. Поверхностно, для списков по числовым индексам */
+--   [F] bool     table.equal2( table t1, table t2 )   /* Проверяет, являются ли таблицы одинаковыми. Рекурсивно, для словарей по всем индексам и ключам */
+--   [F] bool     table.empty( table )      /* Проверяет, является ли таблица пустой */
+--   [F] table    table.clear( table )      /* Очищает существующую таблицу, чтобы не создавать новую. Проходит по числовым индексам таблицы - в таблице не должно быть "дырок": [local t = {[1] = 1, [3] = 3} --> внутри таблицы t нет второго индекса (он уже nil)] = на этой дырке цикл остановится! */
+--   [F] table    table.clear2( table )     /* Очищает существующую таблицу, чтобы не создавать новую. Проходит по всем индексам и ключам таблицы, очищая таблицу целиком вне зависимости от наличия "дырок", жертвуя скоростью по сравнению с [table.clear()] */
+--   [F] string   table.tostring( table )   /* Преобразует таблицу в строку. Рекурсивно, для списков по числовым индексам */
+--   [F] string   table.tostring2( table )  /* Преобразует таблицу в строку. Рекурсивно, для словарей по всем индексам и ключам */
+--   [F] string   table.value( table, any value )   /* Проверяет, содержит ли таблица значение. Возвращает его ключ, если есть. Рекурсивно, для словарей по всем индексам и ключам */
+--   [F] AIParam  table.key( table, string key )    /* Проверяет, содержит ли таблица ключ. Возвращает его значение, если есть. Рекурсивно, для словарей по всем индексам и ключам */
+--   [F] int      table.amount( table, any item )   /* Считает количество значений в таблице. Рекурсивно, для списков по числовым индексам таблицы */
 --   [F] intK&intV     table.amount2( table, any item )   /* Считает количество ключей и значений в таблице. Рекурсивно, для словарей по всем индексам и ключам */
 --
 --
@@ -183,10 +188,10 @@
 --       }
 --
 --       /* Файлы */
---       [M] string file_read( string path )     /* Возвращает содержимое файла как строку */
---       [M] table file_lines( string path )     /* Возвращает содержимое файла как список строк */
---       [M] bool file_exists( string path )     /* Проверяет, существует ли файл по этому пути */
---       [M] bool file_open( file descriptor )   /* Проверяет, открыт ли файл в памяти по этому дескриптору */
+--       [M] string  file_read( string path )      /* Возвращает содержимое файла как строку */
+--       [M] table   file_lines( string path )     /* Возвращает содержимое файла как список строк */
+--       [M] bool    file_exists( string path )    /* Проверяет, существует ли файл по этому пути */
+--       [M] bool    file_open( file descriptor )  /* Проверяет, открыт ли файл в памяти по этому дескриптору */
 --   }
 -- ]]
 --
@@ -267,7 +272,7 @@
 
 local LuaE = {}
 LuaE.__index = LuaE
-LuaE.version = "v2.3"
+LuaE.version = "v2.4"
 LuaE.try = {}
 LuaE.freezed_code = {}
 local freeze = LuaE.freezed_code
@@ -280,9 +285,15 @@ local str_gsub = string.gsub
 local str_low = string.lower
 local str_find = string.find
 local str_format = string.format
+local str_byte = string.byte
 
 local t_insert = table.insert
+local t_concat = table.concat
 local t_getn = table.getn
+local t_setn = table.setn
+local next = next
+local pairs = pairs
+local ipairs = ipairs
 
 local io_open = io.open
 
@@ -291,13 +302,21 @@ LOG("[I] Init Module LuaExtended.lua ...")
 
 
 LuaE.shield = {
+    ["\0"] = "%0",
     ["\\"] = "%\\",
     ["\""] = "%\"",
+
+    ["\t"] = "%\t",
+    ["\n"] = "%\n",
+    ["\r"] = "%\r",
+
     ["'"] = "%'",
+
     ["["] = "%[",
     ["]"] = "%]",
     ["("] = "%(",
     [")"] = "%)",
+
     ["."] = "%.",
     ["^"] = "%^",
     ["$"] = "%$",
@@ -307,6 +326,102 @@ LuaE.shield = {
     ["?"] = "%?",
     ["%"] = "%%"
 }
+local shield = LuaE.shield
+
+LuaE.unshield = {
+    ["%0"] = "\0",
+    ["%\\"] = "\\",
+    ["%\""] = "\"",
+
+    ["%\t"] = "\t",
+    ["%\n"] = "\n",
+    ["%\r"] = "\r",
+
+    ["%'"] = "'",
+
+    ["%["] = "[",
+    ["%]"] = "]",
+    ["%("] = "(",
+    ["%)"] = ")",
+    
+    ["%."] = ".",
+    ["%^"] = "^",
+    ["%$"] = "$",
+    ["%*"] = "*",
+    ["%+"] = "+",
+    ["%-"] = "-",
+    ["%?"] = "?",
+    ["%%"] = "%"
+}
+local unshield = LuaE.unshield
+
+LuaE.hex = {
+    [0]   = "00", [1]   = "01", [2]   = "02", [3]   = "03",
+    [4]   = "04", [5]   = "05", [6]   = "06", [7]   = "07",
+    [8]   = "08", [9]   = "09", [10]  = "0A", [11]  = "0B",
+    [12]  = "0C", [13]  = "0D", [14]  = "0E", [15]  = "0F",
+    [16]  = "10", [17]  = "11", [18]  = "12", [19]  = "13",
+    [20]  = "14", [21]  = "15", [22]  = "16", [23]  = "17",
+    [24]  = "18", [25]  = "19", [26]  = "1A", [27]  = "1B",
+    [28]  = "1C", [29]  = "1D", [30]  = "1E", [31]  = "1F",
+    [32]  = "20", [33]  = "21", [34]  = "22", [35]  = "23",
+    [36]  = "24", [37]  = "25", [38]  = "26", [39]  = "27",
+    [40]  = "28", [41]  = "29", [42]  = "2A", [43]  = "2B",
+    [44]  = "2C", [45]  = "2D", [46]  = "2E", [47]  = "2F",
+    [48]  = "30", [49]  = "31", [50]  = "32", [51]  = "33",
+    [52]  = "34", [53]  = "35", [54]  = "36", [55]  = "37",
+    [56]  = "38", [57]  = "39", [58]  = "3A", [59]  = "3B",
+    [60]  = "3C", [61]  = "3D", [62]  = "3E", [63]  = "3F",
+    [64]  = "40", [65]  = "41", [66]  = "42", [67]  = "43",
+    [68]  = "44", [69]  = "45", [70]  = "46", [71]  = "47",
+    [72]  = "48", [73]  = "49", [74]  = "4A", [75]  = "4B",
+    [76]  = "4C", [77]  = "4D", [78]  = "4E", [79]  = "4F",
+    [80]  = "50", [81]  = "51", [82]  = "52", [83]  = "53",
+    [84]  = "54", [85]  = "55", [86]  = "56", [87]  = "57",
+    [88]  = "58", [89]  = "59", [90]  = "5A", [91]  = "5B",
+    [92]  = "5C", [93]  = "5D", [94]  = "5E", [95]  = "5F",
+    [96]  = "60", [97]  = "61", [98]  = "62", [99]  = "63",
+    [100] = "64", [101] = "65", [102] = "66", [103] = "67",
+    [104] = "68", [105] = "69", [106] = "6A", [107] = "6B",
+    [108] = "6C", [109] = "6D", [110] = "6E", [111] = "6F",
+    [112] = "70", [113] = "71", [114] = "72", [115] = "73",
+    [116] = "74", [117] = "75", [118] = "76", [119] = "77",
+    [120] = "78", [121] = "79", [122] = "7A", [123] = "7B",
+    [124] = "7C", [125] = "7D", [126] = "7E", [127] = "7F",
+    [128] = "80", [129] = "81", [130] = "82", [131] = "83",
+    [132] = "84", [133] = "85", [134] = "86", [135] = "87",
+    [136] = "88", [137] = "89", [138] = "8A", [139] = "8B",
+    [140] = "8C", [141] = "8D", [142] = "8E", [143] = "8F",
+    [144] = "90", [145] = "91", [146] = "92", [147] = "93",
+    [148] = "94", [149] = "95", [150] = "96", [151] = "97",
+    [152] = "98", [153] = "99", [154] = "9A", [155] = "9B",
+    [156] = "9C", [157] = "9D", [158] = "9E", [159] = "9F",
+    [160] = "A0", [161] = "A1", [162] = "A2", [163] = "A3",
+    [164] = "A4", [165] = "A5", [166] = "A6", [167] = "A7",
+    [168] = "A8", [169] = "A9", [170] = "AA", [171] = "AB",
+    [172] = "AC", [173] = "AD", [174] = "AE", [175] = "AF",
+    [176] = "B0", [177] = "B1", [178] = "B2", [179] = "B3",
+    [180] = "B4", [181] = "B5", [182] = "B6", [183] = "B7",
+    [184] = "B8", [185] = "B9", [186] = "BA", [187] = "BB",
+    [188] = "BC", [189] = "BD", [190] = "BE", [191] = "BF",
+    [192] = "C0", [193] = "C1", [194] = "C2", [195] = "C3",
+    [196] = "C4", [197] = "C5", [198] = "C6", [199] = "C7",
+    [200] = "C8", [201] = "C9", [202] = "CA", [203] = "CB",
+    [204] = "CC", [205] = "CD", [206] = "CE", [207] = "CF",
+    [208] = "D0", [209] = "D1", [210] = "D2", [211] = "D3",
+    [212] = "D4", [213] = "D5", [214] = "D6", [215] = "D7",
+    [216] = "D8", [217] = "D9", [218] = "DA", [219] = "DB",
+    [220] = "DC", [221] = "DD", [222] = "DE", [223] = "DF",
+    [224] = "E0", [225] = "E1", [226] = "E2", [227] = "E3",
+    [228] = "E4", [229] = "E5", [230] = "E6", [231] = "E7",
+    [232] = "E8", [233] = "E9", [234] = "EA", [235] = "EB",
+    [236] = "EC", [237] = "ED", [238] = "EE", [239] = "EF",
+    [240] = "F0", [241] = "F1", [242] = "F2", [243] = "F3",
+    [244] = "F4", [245] = "F5", [246] = "F6", [247] = "F7",
+    [248] = "F8", [249] = "F9", [250] = "FA", [251] = "FB",
+    [252] = "FC", [253] = "FD", [254] = "FE", [255] = "FF"
+}
+local hex = LuaE.hex
 
 
 if not string.match then
@@ -359,16 +474,9 @@ end
 if not string.shield then
     function string.shield(str, boolReverse)
         if boolReverse then
-            if str_find(str, "%%%%") then
-                str = str_gsub(str, "%%%%", "||")
-                str = str_gsub(str, "%%", "")
-                str = str_gsub(str, "||", "%%")
-            else
-                str = str_gsub(str, "%%", "")
-            end
-            return str
+            return str_gsub(str, "%%(.)", function(c) return unshield[c] or c end)
         else
-            return str_gsub(str, ".", function(char) return LuaE.shield[char] or char end)
+            return str_gsub(str, ".", function(c) return shield[c] or c end)
         end
     end
 end
@@ -385,6 +493,30 @@ if not string.commas then
     function string.commas(str)
         local _, commas = str_gsub(str, ",", ",")
         return commas or 0
+    end
+end
+if not string.isbinary then
+    function string.isbinary(str)
+        if str_find(str, "[%z\1-\8\11\12\14-\31]") then
+            return "<binary:" .. str_len(str) .. " bytes>" --true
+        end
+        return false
+    end
+end
+if not string.hex then
+    function string.hex(str, separator)
+        local length = str_len(str)
+        local result = {}
+        local n = 1
+        for i = 1, length do
+            if separator and i > 1 then
+                result[n] = separator
+                n = n + 1
+            end
+            result[n] = hex[str_byte(str, i)]
+            n = n + 1
+        end
+        return t_concat(result)
     end
 end
 
@@ -420,6 +552,15 @@ if not table.copy then
             copy = orig
         end
         return copy
+    end
+end
+if not table.getn2 then
+    function table.getn2(tbl)
+        local n = 0 
+        for k in pairs(tbl) do 
+            n = n + 1 
+        end 
+        return n
     end
 end
 if not table.equal then
@@ -470,7 +611,7 @@ if not table.clear then
         for i = 1, n do
             tbl[i] = nil
         end
-        table.setn(tbl, 0)
+        t_setn(tbl, 0)
         return tbl
     end
 end
@@ -479,39 +620,55 @@ if not table.clear2 then
         for k in pairs(tbl) do
             tbl[k] = nil
         end
-        table.setn(tbl, 0)
+        t_setn(tbl, 0)
         return tbl
     end
 end
+
+local function escape_str(s)
+    local bin = string.isbinary(s)
+    if bin then
+        return "'" .. bin .. "'"
+    end
+
+    s = str_gsub(s, '"', "'")
+    return "'" .. s .. "'"
+end
 if not table.tostring then
     function table.tostring(tbl)
-        local function escape_str(s)
-            s = string.gsub(s, '"', "'")
-            return "'" .. s .. "'"
-        end
+        local seen = {}
 
         local function serialize(tbl)
-            local result = "{"
             if type(tbl)~="table" then
-                return "nil"
+                --'{"idi nahui eto ne massiff)))0)"}'
+                return "nil" 
             end
-            for i = 1, getn(tbl) do
+            if seen[tbl] then
+                return "'<recursive " .. tostring(tbl) .. ">'"
+            end
+            seen[tbl] = true
+
+            local result = "{"
+            local skoka = t_getn(tbl)
+            for i = 1, skoka do
                 local v = tbl[i]
                 local vtype = type(v)
                 if vtype == "table" then
                     result = result .. serialize(v)
-                elseif vtype == "number" then
-                    result = result .. v
                 elseif vtype == "string" then
                     result = result .. escape_str(v)
-                else
-                    --result = '{"idi nahui eto ne massiff)))0)"}'
+                elseif vtype == "number" then
+                    result = result .. v
+                elseif vtype == "boolean" then
                     result = result .. tostring(v)
+                else
+                    result = result .. "'<" .. vtype .. ">'"
                 end
-                if i < getn(tbl) then
+                if i < skoka then
                     result = result .. ","
                 end
             end
+
             result = result .. "}"
             if result=="{}" then
                 result = "nil"
@@ -522,6 +679,54 @@ if not table.tostring then
         return serialize(tbl)
     end
 end
+if not table.tostring2 then
+    function table.tostring2(tbl)
+        local seen = {}
+
+        local function serialize(tbl)
+            if type(tbl)~="table" then
+                --'{"idi nahui eto ne massiff)))0)"}'
+                return "nil" 
+            end
+            if seen[tbl] then
+                return "'<recursive " .. tostring(tbl) .. ">'"
+            end
+            seen[tbl] = true
+
+            local result = "{"
+            local first = true
+            for k, v in pairs(tbl) do
+                if not first then
+                    result = result .. ","
+                end
+                first = false
+
+                local vtype = type(v)
+                local k_str = "['" .. k .. "']="
+                if vtype == "table" then
+                    result = result .. k_str .. serialize(v)
+                elseif vtype == "string" then
+                    result = result .. k_str .. escape_str(v)
+                elseif vtype == "number" then
+                    result = result .. k_str .. v
+                elseif vtype == "boolean" then
+                    result = result .. k_str .. tostring(v)
+                else
+                    result = result .. k_str .. "'<" .. vtype .. ">'"
+                end
+            end
+
+            result = result .. "}"
+            if result=="{}" then
+                result = "nil"
+            end
+            return result
+        end
+
+        return serialize(tbl)
+    end
+end
+
 if not table.value then
     function table.value(tbl, value)
         for k, v in pairs(tbl) do 
@@ -564,21 +769,25 @@ if not table.amount then
     end
 end
 if not table.amount2 then
-    function table.amount2(tbl, item, keys, values)
-        keys = keys or 0
-        values = values or 0
-        for k, v in pairs(tbl) do 
-            if type(v) == "table" and v ~= item then 
-                keys, values = table.amount(v, item, keys, values)
+    function table.amount2(tbl, item)
+        local keys, values = 0, 0
+
+        local function serialize(tbl)
+            for k, v in pairs(tbl) do 
+                if type(v) == "table" and v ~= item then 
+                    keys, values = serialize(v)
+                end
+                if k == item then
+                    keys = keys + 1
+                end
+                if v == item then
+                    values = values + 1
+                end
             end
-            if k == item then
-                keys = keys + 1
-            end
-            if v == item then
-                values = values + 1
-            end
+            return keys, values
         end
-        return keys, values
+
+        return serialize(tbl)
     end
 end
 
